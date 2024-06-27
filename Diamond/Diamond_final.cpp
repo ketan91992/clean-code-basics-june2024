a0e46e8665b62ca23b32f8db4238bdfd368184cd
ketan91992 <ketan.tarkas9@gmail.com>
2024-06-27 10:15:28 +0530

    Added Comments in the code

--- Diamond/Diamond_final.cpp
+++ Diamond/Diamond_final.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 //contract , interface , abstraction
+//Abstract Class
+//These interfaces define pure virtual functions that must be implemented by derived classes.
 class IPrintableDevice{
      public:
      virtual void print()=0;
 };
+
+//Abstract Class
+//These interfaces define pure virtual functions that must be implemented by derived classes.
 class IScanner{
    public:
    virtual void scan()=0;
@@ -13,6 +18,8 @@ class D
     private:
         int val;
     protected:
+        //Protected Constructors
+        //Used in the D class to ensure it can only be instantiated by derived classes.
         D(int x) { val = x; std::cout << "D Constructor, val = " << val << std::endl;}
     public:
         ~D() { std::cout << "D destructor called " << std::endl;}
@@ -38,31 +45,42 @@ class PS : public D,public IPrintableDevice,public IScanner
     IScanner* sObj;
     IPrintableDevice* pObj;
     public:
+        //Dependency Injection
+        //Constructor Injection: Used in the PS class to inject dependencies
         PS(int x,IScanner *sAddress,IPrintableDevice *pAddress) :
         D(x), sObj{sAddress},pObj{pAddress} { std::cout << "PS Constructor" << std::endl;
 
         }
         ~PS() { std::cout << "PS destructor called " << std::endl;}
+        //Delegation
+        //PS class delegates the print and scan tasks to the injected objects
         void print(){
             pObj.print();
         }
+        //Delegation
+        //PS class delegates the print and scan tasks to the injected objects
         void scan(){
             sObj.scan();
         }
 };
+
+//Runtime Polymorphism: Achieved through virtual functions and pointers to base classes.
 void printTask(IPrintableDevice* ptr)
 {
     ptr->print();
 }
+
 template<typename T>
 void print(T* ptr){
      ptr->print();
 }
 
+//Runtime Polymorphism: Achieved through virtual functions and pointers to base classes.
 void scanTask(IScanner* sptr)
 {
     sptr->scan();
 }
+
 int main()
 {
     P pObj;
